0ebb899608cd3c716c599cadec2bc25bd396fe69
dinoxas <dhassiotis07@gmail.com>
2020-05-17 19:43:53 +0100

    cretaed Queries for fetching jobs and companies and use data to populate state

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,18 @@
+const db = require('./db');
+
 const Query = {
-  greeting: () => 'Hello world!'
+  company: (root, { id }) => db.companies.get(id),
+  job: (root, { id }) => db.jobs.get(id),
+  jobs: () => db.jobs.list()
 };
 
-module.exports = { Query };
+const Company = {
+  jobs: (company) =>
+    db.jobs.list().filter((job) => job.companyId === company.id)
+};
+
+const Job = {
+  company: (job) => db.companies.get(job.companyId)
+};
+
+module.exports = { Query, Company, Job };
